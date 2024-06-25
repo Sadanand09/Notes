baa094a0bb8bd06bcb3621536509e71cf15d91b9
sujatagunale <sujata@jsmastery.pro>
2024-06-25 17:21:34 +0530

    fix: redirect on note update

--- client/src/view/UpdateNote/UpdateNote.js
+++ client/src/view/UpdateNote/UpdateNote.js
@@ -1,97 +1,103 @@
-import React, { useEffect, useState } from 'react'
-import '../NewNote/NewNote.css'
-import './UpdateNote.css'
-import axios from 'axios'
-import toast from 'react-hot-toast'
-import { useParams } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import "../NewNote/NewNote.css";
+import "./UpdateNote.css";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { useParams, useNavigate } from "react-router-dom";
 
 function NewNote() {
+  const navigate = useNavigate();
 
-    const [title, setTitle] = useState('');
-    const [category, setCategory] = useState('');
-    const [content, setContent] = useState('');
+  const [title, setTitle] = useState("");
+  const [category, setCategory] = useState("");
+  const [content, setContent] = useState("");
 
+  const loadNote = async (id) => {
+    if (!id) return;
 
-    const loadNote = async (id) => {
-        if (!id) return
+    const response = await axios.get(
+      `${process.env.REACT_APP_API_URL}/notes/${id}`
+    );
 
-        const response = await axios.get(`${process.env.REACT_APP_API_URL}/notes/${id}`)
+    setTitle(response.data.data.title);
+    setCategory(response.data.data.category);
+    setContent(response.data.data.content);
+  };
 
-        setTitle(response.data.data.title)
-        setCategory(response.data.data.category)
-        setContent(response.data.data.content)
-    }
+  const UpdateNote = async () => {
+    console.log("coming here");
 
-    const UpdateNote = async () => {
-        const response = await axios.put(`${process.env.REACT_APP_API_URL}/notes/${id}`,{
-                title: title,
-                content: content,
-                category: category
-            })
+    const response = await axios.put(
+      `${process.env.REACT_APP_API_URL}/notes/${id}`,
+      {
+        title: title,
+        content: content,
+        category: category,
+      }
+    );
 
-        toast.success(response.data.message)
+    console.log({ response });
 
-        window.location.href = "/"
-    }
+    toast.success(response.data.message);
 
-    const { id } = useParams();
+    // navigate to home
+    navigate("/");
+  };
 
-    useEffect(() => {
-        loadNote(id);
-    }, [id])
+  const { id } = useParams();
 
+  useEffect(() => {
+    loadNote(id);
+  }, [id]);
 
+  return (
+    <div>
+      <h1 className="app-header">Update Note</h1>
 
+      <form className="form-new-note">
+        <input className="input-id" type="text" value={id} disabled />
 
+        <input
+          type="text"
+          placeholder="Title"
+          value={title}
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+          className="input-title"
+        />
 
-    return (
-        <div>
-            <h1 className='app-header'>Update Note</h1>
+        <select
+          value={category}
+          onChange={(e) => {
+            setCategory(e.target.value);
+          }}
+          className="input-category"
+        >
+          <option value="">Select a category</option>
+          <option value="general">General</option>
+          <option value="work">Work</option>
+          <option value="personal">Personal</option>
+          <option value="learning">Learning</option>
+          <option value="other">Other</option>
+        </select>
 
-            <form className='form-new-note'>
+        <input
+          type="text"
+          placeholder="Content"
+          value={content}
+          className="input-content"
+          onChange={(e) => {
+            setContent(e.target.value);
+          }}
+        />
 
-                <input className='input-id' type='text' value={id} disabled />
-
-                <input type='text'
-                    placeholder="Title"
-                    value={title}
-                    onChange={(e) => {
-                        setTitle(e.target.value)
-                    }}
-                    className='input-title'
-                />
-
-                <select value={category}
-                    onChange={(e) => {
-                        setCategory(e.target.value)
-                    }}
-                    className='input-category'>
-                    <option value="">Select a category</option>
-                    <option value="general">General</option>
-                    <option value="work">Work</option>
-                    <option value="personal">Personal</option>
-                    <option value="learning">Learning</option>
-                    <option value="other">Other</option>
-                </select>
-
-                <input type='text'
-                    placeholder="Content"
-                    value={content}
-                    className='input-content'
-                    onChange={(e) => {
-                        setContent(e.target.value)
-                    }} />
-
-                <button
-                    type='button'
-                    onClick={UpdateNote}
-                    className='btn'
-                >
-                    Update
-                </button>
-            </form>
-        </div>
-    )
+        <button type="button" onClick={UpdateNote} className="btn">
+          Update
+        </button>
+      </form>
+    </div>
+  );
 }
 //asdf
-export default NewNote
+export default NewNote;
--- server/index.js
+++ server/index.js
@@ -1,124 +1,124 @@
-import express from 'express';
-import cors from 'cors';
-import mongoose from 'mongoose';
-import Note from './models/Note.js';
-import dotenv from 'dotenv';
-dotenv.config(); 
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import Note from "./models/Note.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-const connectDB = async()=>{
-    await mongoose.connect(process.env.MONGODB_URL)
-    console.log("Database connected")
-
-}
+const connectDB = async () => {
+  await mongoose.connect(process.env.MONGODB_URL);
+  console.log("Database connected");
+};
 connectDB();
 
 const PORT = process.env.PORT || 5000;
 
+app.get("/health", (req, res) => {
+  res.json({
+    success: true,
+    message: "Server is running",
+    data: null,
+  });
+});
 
+app.post("/notes", async (req, res) => {
+  const { title, content, category } = req.body;
 
+  if (!title) {
+    return res.json({
+      success: false,
+      message: "Title is required",
+      data: null,
+    });
+  }
+  if (!content) {
+    return res.json({
+      success: false,
+      message: "Content is required",
+      data: null,
+    });
+  }
+  if (!category) {
+    return res.json({
+      success: false,
+      message: "Category is required",
+      data: null,
+    });
+  }
 
-app.get('/health', (req, res)=>{
-    res.json({
-        success: true,
-        message: "Server is running",
-        data: null
-    })
-    
-})
+  const newNote = await Note.create({
+    title: title,
+    content: content,
+    category: category,
+  });
 
-app.post('/notes', async(req, res)=> {
-    const{title, content, category} = req.body;
+  res.json({
+    success: true,
+    message: "Note created successfully",
+    data: newNote,
+  });
+});
 
-    if(!title){
-        return res.json({
-            success: false,
-            message: "Title is required",
-            data: null
-        })
-    }
-    if(!content){
-        return res.json({
-            success: false,
-            message: "Content is required",
-            data: null
-        })
-    }
-    if(!category){
-        return res.json({
-            success: false,
-            message: "Category is required",
-            data: null
-        })
-    }
+app.get("/notes", async (req, res) => {
+  const notes = await Note.find();
 
-    const newNote = await Note.create({
+  res.json({
+    success: true,
+    message: "Notes fetched successfully",
+    data: notes,
+  });
+});
 
-        "title": title,
-        "content": content,
-        "category": category
-    })
+app.get("/notes/:id", async (req, res) => {
+  const { id } = req.params;
 
+  const note = await Note.findById(id);
 
-    res.json({
-        success: true,
-        message: "Note created successfully",
-        data: newNote
-    })
-})
+  res.json({
+    success: true,
+    message: "Note fetched successfully",
+    data: note,
+  });
+});
 
-app.get('/notes', async(req, res)=>{
+app.put("/notes/:id", async (req, res) => {
+  const { id } = req.params;
 
-    const notes = await Note.find();
+  const { title, content, category } = req.body;
 
-    res.json({
-        success: true,
-        message: "Notes fetched successfully",
-        data: notes
-    })  
-
-})
-
-app.get('/notes/:id', async(req, res)=>{
-    const {id} = req.params;
-
-    const note = await Note.findById(id);
-
-    res.json({
-        success: true,
-        message: "Note fetched successfully",
-        data: note
-    })
-})
-
-app.put("/notes/:id", async(req, res)=>{
-    const {id} = req.params;
-
-    const {title, content, category} = req.body;
-
-    await Note.updateOne({_id:id}, {$set:{
+  await Note.updateOne(
+    { _id: id },
+    {
+      $set: {
         title: title,
         content: content,
-        category: category
-    }})
-})
+        category: category,
+      },
+    }
+  );
 
-app.delete("/notes/:id", async(req, res) =>{
-    const {id} = req.params;
-    await Note.deleteOne({_id: id})
+  res.json({
+    success: true,
+    message: "Note updated successfully",
+    data: null,
+  });
+});
 
-    res.json({
-        success: true,
-        message: "Note deleted successfully",
-        data: null
-        })
-})
-        
+app.delete("/notes/:id", async (req, res) => {
+  const { id } = req.params;
+  await Note.deleteOne({ _id: id });
 
+  res.json({
+    success: true,
+    message: "Note deleted successfully",
+    data: null,
+  });
+});
 
-app.listen(PORT, ()=>{
-    console.log(`Server is running on PORT ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT ${PORT}`);
 });
