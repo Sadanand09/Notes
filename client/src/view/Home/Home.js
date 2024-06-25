fcb69e3fca129056136687b0477c172cc1296d19
Sadanand09 <sadanandgurude9@gmail.com>
2024-06-25 08:35:04 +0530

    push

--- client/src/view/Home/Home.js
+++ client/src/view/Home/Home.js
@@ -1,13 +1,13 @@
-import React, {useEffect, usestate} from 'react'
+import React, {useEffect, useState} from 'react'
 import axios from 'axios';
 import './Home.css'
 
 function Home() {
-    const [notes, setNotes] = usestate([]);
+    const [notes, setNotes] = useState([]);
 
     const loadNotes = async ()=>{
 
-        const response = await axios.get('http://localhost:5000/notes')
+        const response = await axios.get(`${process.enc.REACT_APP_API_URL}/notes`)
         setNotes(response.data.data);
 
 
@@ -19,14 +19,15 @@ function Home() {
 
   return (
     <div>
-      Home
-
+      
       {
         notes.map((note, index)=>{
             return(
                 <div>
 
                     <h3> {note.title} </h3>
+                    <p> {note.content} </p>
+                    <span>{note.category}</span>
 
                 </div>
             )
